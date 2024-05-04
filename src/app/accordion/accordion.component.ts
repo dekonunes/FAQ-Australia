13868593411a13f8651c850dacb5c44a12e7bccd
Andre Nunes <anunes@canibuild.com>
2024-05-04 19:05:47 +1000

    removed analitycs

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -2,28 +2,34 @@ import { Component } from '@angular/core';
 import { AccordionService } from './accordion.service';
 import { Accordion } from './accordion';
 import { ActivatedRoute, Params } from '@angular/router';
-import { GoogleAnalyticsService } from 'ngx-google-analytics';
+// import { GoogleAnalyticsService } from 'ngx-google-analytics';
 
 @Component({
-	selector: 'app-accordion',
-	templateUrl: './accordion.component.html',
-	styleUrls: ['./accordion.component.scss']
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.scss'],
 })
 export class AccordionComponent {
-	accordions: Accordion[];
-	params: Params;
+  accordions: Accordion[];
+  params: Params;
 
-	constructor(private svc: AccordionService, private route: ActivatedRoute, private gaService: GoogleAnalyticsService) {
-		svc.getData()
-			.subscribe((accordions) =>
-				{
-					this.accordions = accordions;
-					setTimeout(() => this.route.queryParams.subscribe((params: Params) => this.params = params));
-					// ?item=vistos&subitem=bridge
-				});
-	}
+  constructor(
+    private svc: AccordionService,
+    private route: ActivatedRoute
+  ) // private gaService: any
+  {
+    svc.getData().subscribe((accordions) => {
+      this.accordions = accordions;
+      setTimeout(() =>
+        this.route.queryParams.subscribe(
+          (params: Params) => (this.params = params)
+        )
+      );
+      // ?item=vistos&subitem=bridge
+    });
+  }
 
-	gaTrack(category: string, action: string, item?: string) {
-		this.gaService.event(category, action, item);
-	}
+  // gaTrack(category: string, action: string, item?: string) {
+  // 	this.gaService.event(category, action, item);
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 import { AccordionModule } from './accordion/accordion.module';
-import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
+// import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
@@ -21,7 +21,7 @@ import { HeaderComponent } from './header/header.component';
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
     }),
-    NgxGoogleAnalyticsModule.forRoot('UA-143816400-2'),
+    // NgxGoogleAnalyticsModule.forRoot('UA-143816400-2'),
   ],
   providers: [],
   bootstrap: [AppComponent],
